129f88082a7fff28445f719a4941f7695031f3c0
Asada678 <55794572+Asada678@users.noreply.github.com>
2020-12-02 13:05:09 +0900

    add scrollTrigger

--- scripts/main.js
+++ scripts/main.js
@@ -15,18 +15,45 @@ navItems.forEach(item => {
     }
   });
 });
+console.log('ScrollTrigger:', ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger)
 
 const tl = gsap.timeline();
+const tlHero = gsap.timeline({
+  scrollTrigger: { trigger: '#hero' }
+});
+const tlAbout = gsap.timeline({
+  scrollTrigger: { trigger: '#about' }
+});
+
 tl
   .to('.overlay', {
     y: '100%',
     duration: 1,
     stagger: .3
   })
+  .from('header', {
+    y: '-100%',
+    opacity: 0,
+    duration: .5,
+  })
+  .from('.logo', {
+    x: -60,
+    opacity: 0,
+    duration: .7,
+  }, '-=.2')
+  .from('.menu', {
+    x: 60,
+    opacity: 0,
+    duration: .7
+  }, '-=.2');
+
+tlHero
   .from('.hero-clipped', {
     scaleX: 0,
     x: '100%',
-    duration: 1
+    duration: 1,
+    scrollTrigger: { trigger: '.hero' }
   })
   .from('.hero-textbox h1', {
     y: 60,
@@ -43,18 +70,9 @@ tl
     opacity: 0,
     duration: .7,
   }, '-=.1')
-  .from('header', {
-    y: '-100%',
+
+  tlAbout.from('#about h2', {
+    y: 200,
     opacity: 0,
-    duration: .7
-  })
-  .from('.logo', {
-    x: -60,
-    opacity: 0,
-    duration: .7
-  })
-  .from('.menu', {
-    x: 60,
-    opacity: 0,
-    duration: .7
-  }, '-=.2')
+    duration: .7,
+  }, '+=.3')
