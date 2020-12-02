57bf5f28d881bc9902dc496958d2be100adaf2bc
Asada678 <55794572+Asada678@users.noreply.github.com>
2020-12-02 11:22:07 +0900

    add files

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const navbar = document.querySelector('.navbar');
+const menu = document.querySelector('.menu');
+
+menu.addEventListener('click', () => {
+  navbar.classList.toggle('open')
+});
+
+const tl = gsap.timeline();
+tl
+  .from('.hero-clipped', {
+    scaleX: 0,
+    x: '100%',
+    duration: 1
+  })
+  .from('.logo', {
+    x: -60,
+    opacity: 0,
+    duration: .7
+  })
+  .from('.menu', {
+    x: 60,
+    opacity: 0,
+    duration: .7
+  }, '-=.5')
+  .from('.hero-textbox h1', {
+    y: 60,
+    opacity: 0,
+    duration: .7,
+  },)
+  .from('.hero-textbox h2', {
+    y: 60,
+    opacity: 0,
+    duration: .7,
+  }, '-=.3')
+  .from('.hero-textbox .cta-btn', {
+    y: 60,
+    opacity: 0,
+    duration: .7,
+  }, '-=.1')
